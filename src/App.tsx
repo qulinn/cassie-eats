da6e1214c76ed4893f3a985cfa9223564e0b3e81
qulinn <linmagari0@gmail.com>
2024-09-27 17:40:03 +0900

    added moving object

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import newImage from './images/cassie.png';
+import newImage from './images/cassie.png'; // 左右に動く画像
+import squareImage from './images/cassie2.png'; // 四角で移動する画像（適切な画像を指定）
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={newImage} className="App-logo" alt="new" />
+        {/* 左右に行き来する画像 */}
+        <img src={newImage} className="App-logo App-moving" alt="moving" />
+        {/* 四角で移動する画像 */}
+        <img src={squareImage} className="App-logo App-square" alt="square" />
         <p>
           Incoming New App : Cassie Eats
-          <br></br>
+          <br />
           Assignee : Cassie
         </p>
       </header>
