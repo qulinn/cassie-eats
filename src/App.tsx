4574a1bdcd9a7a5d89e27cf4cbc27b3e09935be4
qulinn <linmagari0@gmail.com>
2024-09-27 13:58:41 +0900

    assign everything to Cassie :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import newImage from './images/cassies-app.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={newImage} className="App-logo" alt="new" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Incoming New App : Cassie Eats
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Assignee : Cassie
+        </p>
       </header>
     </div>
   );
